34e19f037dae73af692fa10918a876e42cf019f7
yJung252 <hyeye101921@naver.com>
2019-08-19 09:42:16 +0900

    첫번째 계산기 코드입니다.

---
+++ src/test/java/com/example/calculator/CalculatorTest.java
@@ -0,0 +1,57 @@
+package com.example.calculator;
+
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class CalculatorTest {
+    @Test
+    public void test_양수덧셈() {
+        Calculator calc = new Calculator();
+        int result = calc.add(5,4);
+
+       assertEquals("add 함수의 결과는 0이어야 합니다.", 9, result);
+    }
+
+    @Test
+    public void test_음수덧셈() {
+        Calculator calc = new Calculator();
+        int result = calc.add(-5,-4);
+        assertEquals(-9, result);
+
+    }
+
+
+    @Test
+    public void test_덧셈2() {
+        Calculator calc = new Calculator();
+        int result = calc.add(5,4);
+
+        assertNotEquals(0 , result);
+    }
+
+    @Test
+    public void test_정수나눗셈() {
+        Calculator calc = new Calculator();
+        int result = calc.devide(10,5);
+        assertEquals(2 , result);
+    }
+
+    @Test
+    public void test_실수나눗셈() {
+        Calculator calc = new Calculator();
+        double result = calc.devide(10.0,4.0);
+        assertEquals(2.5 , result);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void test_0으로나누기() {
+        Calculator calc = new Calculator();
+        int result = calc.devide(10,0);
+        assertEquals(0 , result);
+    }
+
+
+}
